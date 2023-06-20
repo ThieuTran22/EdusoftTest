7799c358a85910d2d4cc85059789ac6d02efbe31
ThieuTran22 <thieutran411@gmail.com>
2023-06-20 09:58:57 +0700

    Test branch

--- EduTest/cCombine.cs
+++ EduTest/cCombine.cs
@@ -13,7 +13,7 @@
         public static void UnitTest()
         {
             cSinhVien sinhVien = new cSinhVien("002", "1234");
-            //sinhVien.TestXemChuongTrinhDaoTao();
+            //sinhVien.TestXemChuongTrinhDaoTao(); sss
             //sinhVien.TestDanhGiaKetQuaRenLuyen("Tham gia hội thảo ngày 12/06");
             sinhVien.TestDangKyXetTotNghiep(true, "0984123123", "ghi chú");
         }
